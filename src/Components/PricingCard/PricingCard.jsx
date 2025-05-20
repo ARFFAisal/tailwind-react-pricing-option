d5d21fc017edd98756712470f1ce848f86868cc2
ARFFaisal <rafifaisal710213@gmail.com>
2025-05-21 05:18:48 +0600

    card daisyUi commit

--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import Features from './Features';
+import React from "react";
+import Features from "./Features";
 
-
-const PricingCard = ({pricing}) => {
-    const {name,price,description,features} = pricing;
-    return (
-        <div className='flex flex-col border bg-amber-600 rounded-3xl p-5'>
-            <div>
-              <h1 className='text-7xl'>{name}</h1>
-              <p className='text-3xl'>{price}</p>
-        </div>
-        <div className='bg-amber-300 rounded-3xl p-8 mt-5 flex-1 '>
-            <p>{description}</p>
+const PricingCard = ({ pricing }) => {
+  const { name, price,  features } = pricing;
+  return (
+    <div className="flex flex-col border bg-amber-600 rounded-3xl p-5">
+      <div className="card w-96 bg-base-100 shadow-sm">
+        <div className="card-body">
+          <span className="badge badge-xs badge-warning">Most Popular</span>
+          <div className="flex justify-between">
+            <h2 className="text-3xl font-bold">{name}</h2>
+            <span className="text-xl">{price}</span>
+          </div>
+          <ul className="mt-6 flex-1  gap-2 text-xs">
             {
-                features.map((feature,index) => <Features key={index} feature={feature}></Features>)
+                features.map(feature => <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>{feature}</span>
+            </li>)
             }
+
+          </ul>
+          <div className="mt-6">
+            <button className="btn btn-primary btn-block">Subscribe</button>
+          </div>
         </div>
-        <button className="btn w-full">Purchase</button>
-        </div>
-       
-        
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PricingCard;
